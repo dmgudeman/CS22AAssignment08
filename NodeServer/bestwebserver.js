6a08b0d1d6e4734a6d029b65c3bce9f74cdd9fc9
David M Gudeman <davidmgudeman@gmail.com>
2015-02-24 09:10:46 -0800

    finalized webserver and added home.html

--- NodeServer/bestwebserver.js
+++ NodeServer/bestwebserver.js
@@ -1,31 +1,49 @@
 //The following function will be called when the server
 // is handling a request
 function servePage(request, response) {
-    // Read the file asynchronously
-   // The filename is hardcoded here - nodedemo.html is assumed to  be in a separate folder
+    // The file is read asynchronously
+
+    // resource extracts the pathname to an object
     var resource = "." + url.parse(request.url).pathname;
     
+    //if there is no pathname route to homepage
+    if (resource === "./") {
+        resource = './html/home.html';
+    }
+    //otherwise read from file defined in resource object
     fs.readFile(resource, function( err, content) {
+        
+        // extract only the path extension
+        var extension = path.extname(resource);
+
         if (err) { // If there is an error, set the status code
             response.writeHead( 404,
                                {'Content-Type': 'text/plain; charset = UTF-8'});
             response.write( err.message); // Include the error message body
             response.end(); // Done
         } else { // Otherwise, the file was read successfully.
-            
-          
-           /* var extension = path.extname(resource);
-            console.log("extension =" + extension);
-                if (extension === ".html"){
-
-                    return 'text/html; charset = UTF-8';
-                }
-        */
-            console.log("url xx=" + resource);
+             
+             // intialize variable to hold the content type to be used in callback
+            var contentType = "";
+             // determine which content type to use
+            if (extension === ".html"){
+                contentType = 'text/html; charset = UTF-8';
+            }
+            if (extension === ".js"){
+               contentType = 'application/javascript; charset = UTF-8';
+            }
+            if (extension === ".css"){
+                contentType = 'text/css; charset = UTF-8';
+            }
+            if (extension === ".gif"){
+                contentType = 'image/gif';
+            }  
+       
             response.writeHead( 200, // Set the status code
-                               {'Content-Type': 'text/html; charset = UTF-8'});
+                               {'Content-Type': contentType});
+            
             response.write(content); // Send file contents as response body
-            response.end();
+            response.end(); //done
         }
     });   
 }
@@ -33,14 +51,11 @@ function servePage(request, response) {
 var fs = require("fs");
 // load the http module
 var http = require("http");
-
+// load the url module
 var url = require("url");
-
+// load the path module
 var path = require("path");
  
- 
-
-
 
 // create a server object
 var server = http.createServer(servePage);
